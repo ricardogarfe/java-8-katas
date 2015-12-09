147ae70cee59e64fa5667964138c681fc1a6385f
Ricardo García Fernández <rgarcia@excentia.es>
2015-12-09 12:12:53 +0100

    Refactor code and generate basic solution

--- src/main/java/org/ricardogarfe/katas/FizzBuzz.java
+++ src/main/java/org/ricardogarfe/katas/FizzBuzz.java
@@ -2,12 +2,13 @@ package org.ricardogarfe.katas;
 
 import java.util.*;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
 
     private static int FIZZ_VALUE = 3;
-    private static int BUZZ_VALUE = 3;
+    private static int BUZZ_VALUE = 5;
     private static int ZERO_VALUE = 0;
 
     Predicate<Integer> fizzFilter = (value) -> (value % FIZZ_VALUE == ZERO_VALUE);
@@ -15,7 +16,27 @@ public class FizzBuzz {
 
     public void printCountDown(int maxCount) {
         List<Integer> counterList = retrieveArrayFromMaxValue(maxCount);
-        counterList.stream().filter(fizzFilter).forEach((value) -> System.out.println(value));
+
+        counterList.stream().filter(fizzFilter).filter(buzzFilter).collect(Collectors.<Integer>toList()).forEach((value) -> System.out.println(value));
+
+    }
+
+    /**
+     * Non functional result
+     * @param value
+     * @return
+     */
+    public String retrieveFizzBuzz(int value) {
+
+        String result = "";
+
+        if (value % FIZZ_VALUE == 0) {
+            result += "Fizz";
+        }
+        if (value % BUZZ_VALUE == 0) {
+            result += "Buzz";
+        }
+        return result.length() > 0 ? result : Integer.toString(value);
     }
 
     public List<Integer> retrieveArrayFromMaxValue(int maxCount) {
--- src/test/java/org/ricardogarfe/katas/FizzBuzzTest.java
+++ src/test/java/org/ricardogarfe/katas/FizzBuzzTest.java
@@ -2,11 +2,18 @@ package org.ricardogarfe.katas;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class FizzBuzzTest {
 
     static FizzBuzz fizzBuzz;
+
+    private final static String ONE_RESULT = "1";
+    private final static String FIZZ_RESULT = "Fizz";
+    private final static String BUZZ_RESULT = "Buzz";
+    private final static String FIZZ_BUZZ_RESULT = "FizzBuzz";
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
@@ -14,10 +21,22 @@ public class FizzBuzzTest {
     }
 
     @Test
-    public void testShowCountDown() {
+    public void testRetrieveFizzBuzz() {
 
-        fizzBuzz.printCountDown(10);
-        assertTrue("That's true not false",true);
+        String fizzBuzzResult = "";
+
+        fizzBuzzResult = fizzBuzz.retrieveFizzBuzz(1);
+        assertEquals("Result is not 1",ONE_RESULT, fizzBuzzResult);
+
+        fizzBuzzResult = fizzBuzz.retrieveFizzBuzz(3);
+        assertEquals("Result is not Fizz",FIZZ_RESULT, fizzBuzzResult);
+
+        fizzBuzzResult = fizzBuzz.retrieveFizzBuzz(5);
+        assertEquals("Result is not Buzz",BUZZ_RESULT, fizzBuzzResult);
+
+        fizzBuzzResult = fizzBuzz.retrieveFizzBuzz(15);
+        assertEquals("Result is not FizzBuzz", FIZZ_BUZZ_RESULT, fizzBuzzResult);
     }
+
 }
 
