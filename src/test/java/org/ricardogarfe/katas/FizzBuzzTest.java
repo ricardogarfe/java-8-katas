1b6c3823ced56df5947fa5710117d973fffdd89f
Ricardo García Fernández <rgarcia@excentia.es>
2015-12-09 14:44:51 +0100

    Remove unused imports

--- src/test/java/org/ricardogarfe/katas/FizzBuzzTest.java
+++ src/test/java/org/ricardogarfe/katas/FizzBuzzTest.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(Parameterized.class)
 public class FizzBuzzTest {
@@ -47,4 +46,3 @@ public class FizzBuzzTest {
     }
 
 }
-
