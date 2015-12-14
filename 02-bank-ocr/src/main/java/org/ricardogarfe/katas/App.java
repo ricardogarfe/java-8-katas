df3d37a96cebc84a1cbdf9154664b4797c893a00
Ricardo García Fernández <ricardogarfe@gmail.com>
2015-12-14 10:59:07 +0100

    Banck OCR kata first code [Not working] and info.

---
+++ 02-bank-ocr/src/main/java/org/ricardogarfe/katas/App.java
@@ -0,0 +1,13 @@
+package org.ricardogarfe.katas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
